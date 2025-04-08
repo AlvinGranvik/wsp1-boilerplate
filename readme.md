833d9a3d5c8a2db194cec7a28c4effe13bde84e0
AlvinGranvik <alvin.granvik@elev.ga.ntig.se>
2025-04-08 11:19:39 +0200

    Various login changes

--- readme.md
+++ readme.md
@@ -4,3 +4,16 @@
 - Initializerade repo, lade till alla nuvarande filer
 - Installerade några nödvändiga packages
 - Lade till index.js, samt redigerade server.js för att bättre passa mitt projekt
+
+## Lektion 2
+- Views fungerar nu, all setup färdig inför nästa passen
+
+## Lektion 3
+- Login form existerar
+
+## Lektion 4
+- Login page
+
+## Lektion 5 & 6
+- Planering färdig
+- Påbörjat arbete av implementering för själva systemet för att logga in
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import pool from "../db.js"
 import bcrypt from "bcrypt"
 import session from "express-session"
+import user from "../db.js"
 
 const router = express.Router()
 const app = express()
@@ -10,6 +11,12 @@ let myPlaintextPassword = "bibnib"
 bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
 	// här får vi nu tag i lösenordets hash i variabeln hash
 	console.log(hash)
+})
+
+router.get("/login", async (req, res) => { 
+  const [user] = await pool.promise().query(`
+  user.name*
+  `)
 })
 
 router.get("/", async (req, res) => {
@@ -22,17 +29,23 @@ router.get("/", async (req, res) => {
         { title: "Login", message: "Do the thing", views: req.session.views }
       )
 })
-await
-router.get("/login", (req, res) => {
+
+/*router.get("/login", (req, res) => {
   if (user.name && user.password) {
     res.render("login.njk", {
       title: "logged in"
     })
   } else {
-    res.render("index.njk", {
-      title: "Wrong username or password"
-    })
+      if (req.session.views) {
+        req.session.views++
+      } else {
+        req.session.views = 1
+      }
+    res.render("index.njk",
+    { title: "Wrong username or password", views: req.session.views }
+    )
   }
-})
+})*/
+
 
 export default router
